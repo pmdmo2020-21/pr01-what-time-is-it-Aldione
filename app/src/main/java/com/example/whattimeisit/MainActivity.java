d2dca9a8bd8dc3d695896b894276848ff2d87ad9
Antonio <antoniolozano.saladillo@gmail.com>
2020-09-27 19:38:28 +0200

    Commit 1
    Hemos creado añadido la fecha y hora

--- app/src/main/java/com/example/whattimeisit/MainActivity.java
+++ app/src/main/java/com/example/whattimeisit/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.whattimeisit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SimpleDateFormat") //Esto me lo genera Android Studio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat;
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat timeFormat;
+        timeFormat = new SimpleDateFormat("HH:mm");
+
+        TextView lblDate_now = findViewById(R.id.lblDate_now);
+        lblDate_now.setText(dateFormat.format(calendar.getTime()));
+        TextView lblTime_Now = findViewById(R.id.lblTime_now);
+        lblTime_Now.setText(timeFormat.format(calendar.getTime()));
     }
 }
