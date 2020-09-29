0f4e2295ca38407a10cf5b234ec840092e25b368
Antonio <antoniolozano.saladillo@gmail.com>
2020-09-29 15:47:07 +0200

    Commit3
    Correccion del Feedback.

--- app/src/main/java/com/example/whattimeisit/MainActivity.java
+++ app/src/main/java/com/example/whattimeisit/MainActivity.java
@@ -8,25 +8,24 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    @SuppressLint("SimpleDateFormat") //Esto me lo genera Android Studio
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat dateFormat;
-        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        SimpleDateFormat timeFormat;
-        timeFormat = new SimpleDateFormat("HH:mm");
-
-        setupViews(calendar, dateFormat, timeFormat);
+        setupViews();
     }
 
-    private void setupViews(Calendar calendar, SimpleDateFormat dateFormat, SimpleDateFormat timeFormat) {
+    private void setupViews() {
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
         TextView lblDate_now = findViewById(R.id.lblDate_now);
         lblDate_now.setText(dateFormat.format(calendar.getTime()));
         TextView lblTime_Now = findViewById(R.id.lblTime_now);
